f89cfb42aae783e155762651a85aa807c89c3fab
Nonnicha <non@odds.team>
2024-09-12 14:41:35 +0700

    Delete unuse code

--- register-form-RoR/config/routes.rb
+++ register-form-RoR/config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   resources :registrations do
     member do
       get 'confirm_delete'
-      patch 'close_confirm_delete_modal'
     end
   end
-  # delete 'registrations/:id/confirm_delete', to: 'registrations#delete_data', as: 'delete_data'
   root 'registrations#new'
 end
