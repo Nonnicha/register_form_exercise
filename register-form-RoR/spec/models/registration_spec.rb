ec5fa68dfc1f94e51825f0babd3d533eba825e6f
Nonnicha <non@odds.team>
2024-09-16 14:03:39 +0700

    Use Faker with test model

--- register-form-RoR/spec/models/registration_spec.rb
+++ register-form-RoR/spec/models/registration_spec.rb
@@ -1,51 +1,49 @@
-require 'rails_helper'
-
 RSpec.describe Registration, type: :model do
-  it 'Is valid with valid attributes' do
+  it 'กรอกข้อมูลครบทำงานสำเร็จ' do
     registration = Registration.new(
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
-      phone: '1234567890'
+      firstName: Faker::Name.first_name,
+      lastName: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number
     )
     expect(registration).to be_valid
   end
 
-  it 'Is not valid without a firstName' do
+  it 'ไม่กรอกชื่อทำงานไม่สำเร็จ' do
     registration = Registration.new(
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
-      phone: '1234567890'
+      lastName: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:firstName]).to include("can't be blank")
   end
 
-  it 'Is not valid without a lastName' do
+  it 'ไม่กรอกนามสกุลทำงานไม่สำเร็จ' do
     registration = Registration.new(
-      firstName: 'John',
-      email: 'john.doe@example.com',
-      phone: '1234567890'
+      firstName: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:lastName]).to include("can't be blank")
   end
 
-  it 'Is not valid without an email' do
+  it 'ไม่กรอกอีเมลทำงานไม่สำเร็จ' do
     registration = Registration.new(
-      firstName: 'John',
-      lastName: 'Doe',
-      phone: '1234567890'
+      firstName: Faker::Name.first_name,
+      lastName: Faker::Name.last_name,
+      phone: Faker::PhoneNumber.phone_number
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:email]).to include("can't be blank")
   end
 
-  it 'Is not valid without a phone' do
+  it 'ไม่กรอกเบอร์โทรศัพท์ทำงานไม่สำเร็จ' do
     registration = Registration.new(
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com'
+      firstName: Faker::Name.first_name,
+      lastName: Faker::Name.last_name,
+      email: Faker::Internet.email
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:phone]).to include("can't be blank")
