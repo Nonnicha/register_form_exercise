11225d59ff0b5274b8eba3fc726284b2aa20ba36
Nonnicha <non@odds.team>
2024-09-18 10:10:07 +0700

    Add more test for cover the validates

--- register-form-RoR/spec/models/registration_spec.rb
+++ register-form-RoR/spec/models/registration_spec.rb
@@ -25,6 +25,48 @@ RSpec.describe Registration, type: :model do
     expect(registration.errors[:firstName]).to include("Please enter your first name.")
   end
 
+  it 'กรอกชื่อเป็นตัวเลขทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: '1234',
+      lastName: "Wiriyathanachit",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:firstName]).to include("Please use only English letter in first name.")
+  end
+
+  it 'กรอกชื่อเป็นภาษาไทยทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'ชานน',
+      lastName: "Wiriyathanachit",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:firstName]).to include("Please use only English letter in first name.")
+  end
+
+  it 'กรอกชื่อเป็นอักขระพิเศษทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: '@@@@',
+      lastName: "Wiriyathanachit",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:firstName]).to include("Please use only English letter in first name.")
+  end
+
   it 'ไม่กรอกนามสกุลทำงานไม่สำเร็จ' do
     registration = Registration.new(
       firstName: 'Chanon',
@@ -36,6 +78,48 @@ RSpec.describe Registration, type: :model do
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:lastName]).to include("Please enter your last name.")
+  end
+
+  it 'กรอกนามสกุลเป็นตัวเลขทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'Chanon',
+      lastName: "1234",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:lastName]).to include("Please use only English letter in last name.")
+  end
+
+  it 'กรอกนามสกุลเป็นภาษาไทยทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'Chanon',
+      lastName: "วิริยธนชิต",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:lastName]).to include("Please use only English letter in last name.")
+  end
+
+  it 'กรอกนามสกุลเป็นอักขระพิเศษทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'Chanon',
+      lastName: "@@@@",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:lastName]).to include("Please use only English letter in last name.")
   end
 
   it 'ไม่กรอกเพศทำงานไม่สำเร็จ' do
@@ -77,6 +161,20 @@ RSpec.describe Registration, type: :model do
     expect(registration.errors[:email]).to include("Please enter your email.")
   end
 
+  it 'กรอกอีเมลผิดรูปแบบทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'Chanon',
+      lastName: "Wiriyathanachit",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: 'invalidemail',
+      phone: '0987654321',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:email]).to include("Invalid email format.")
+  end
+
   it 'ไม่กรอกเบอร์โทรศัพท์ทำงานไม่สำเร็จ' do
     registration = Registration.new(
       firstName: 'Chanon',
@@ -88,6 +186,20 @@ RSpec.describe Registration, type: :model do
     )
     expect(registration).not_to be_valid
     expect(registration.errors[:phone]).to include("Please enter your phone number.")
+  end
+
+  it 'กรอกเบอร์โทรศัพท์ผิดรูปแบบทำงานไม่สำเร็จ' do
+    registration = Registration.new(
+      firstName: 'Chanon',
+      lastName: "Wiriyathanachit",
+      gender: 'male',
+      birthday: '1999-09-03',
+      email: Faker::Internet.email,
+      phone: '1234',
+      subject: 'Test Subject'
+    )
+    expect(registration).not_to be_valid
+    expect(registration.errors[:phone]).to include("Invalid phone number format.")
   end
 
   it 'ไม่กรอกเรื่องทำงานไม่สำเร็จ' do
